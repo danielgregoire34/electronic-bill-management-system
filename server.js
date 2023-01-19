3fe71c47e117c40614bc2997fd3876878107744c
gmontano79 <gmontano79@gmail.com>
2023-01-19 18:50:00 -0500

    added the route for the public file and added path to the server file

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const sequelize = require('./config/connection');
 const PORT = process.env.PORT || 3000
 const routes = require('./controllers/index');
 const exhpbs = require('express-handlebars');
+const path = require('path');
+
 
 const app = express();
 
@@ -13,7 +15,7 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine','handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 app.listen(PORT, (err) => {
