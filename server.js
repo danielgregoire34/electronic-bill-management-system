8ab8a362faf4244433a6198c5382b27d31bddfc2
unknown <danielgregoire34@gmail.com>
2023-01-18 22:21:14 -0500

    small change to server.js

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const exhpbs = require('express-handlebars');
 
 const app = express();
 
-<<<<<<< HEAD
 const hbs = exhpbs.create({});
 
 app.engine('handlebars', hbs.engine);
@@ -23,12 +22,4 @@ app.listen(PORT, (err) => {
   }
   return console.log(`server is listening on ${PORT}`);
 });
-=======
-app.use((req, res) => {
-  res.status(404).end();
-});
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${port}`)
-})
->>>>>>> c023dc33d0b3d019148ec82bf2238f8652c5537e
