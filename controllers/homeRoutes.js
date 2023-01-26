db63a19d9c751abcdd34ca4484e381afcabf4dd9
unknown <danielgregoire34@gmail.com>
2023-01-25 21:37:04 -0500

    removing html files and converting them to handlebars and connecting homeroutes to all the handlebar pages

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,11 +2,39 @@ const router = require('express').Router();
 
 router.get('/',function(req,res){
     try{
-        res.render('homepage',{title: "SMS"});
+        res.render('login',{title: "Subscription Management Services"});
     } catch (err){
         res.status(500).json(err);
     }
     
 });
+
+router.get('/registration',function(req,res){
+    try{
+        res.render('registration',{title: "Subscription Management Services"});
+    } catch (err){
+        res.status(500).json(err);
+    }
+    
+});
+
+router.get('/dashboard',function(req,res){
+    try{
+        res.render('dashboard',{title: "Subscription Management Services"});
+    } catch (err){
+        res.status(500).json(err);
+    }
+    
+});
+
+router.get('/new-account',function(req,res){
+    try{
+        res.render('new-account',{title: "Subscription Management Services"});
+    } catch (err){
+        res.status(500).json(err);
+    }
+    
+});
+
 
 module.exports = router;
